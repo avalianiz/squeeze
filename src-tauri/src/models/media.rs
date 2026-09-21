5027e0810c722d32966b9320912640c8213d30c3
avalianiz <avalianizurab2006@gmail.com>
2026-09-21 15:03:43 +0400

    feat(models): add a media model so i have a normalized representation for media since ffprobe can return massive json

---
+++ src-tauri/src/models/media.rs
@@ -0,0 +1,10 @@
+pub struct Media {
+    pub path: String,
+    pub duration_seconds: f64,
+    pub size_bytes: u64,
+    pub width: Option<u32>,
+    pub height: Option<u32>,
+    pub frame_rate: Option<f64>,
+    pub video_codec: Option<String>,
+    pub audio_codec: Option<String>,
+}
